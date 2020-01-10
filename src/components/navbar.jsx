7b20e19ad5c7e4dfa887412fdf4e4b15fd95ef7e
urjit2498 <urjit64@gmail.com>
2020-01-10 13:51:51 +0530

    Pushed local repo of react app

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+// Stateless Functional Components
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar - Rendered");
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
